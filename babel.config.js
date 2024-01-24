ff1f6ddf881f0be1e64a6e4cd0ca7b45ad4a8e94
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-01-23 20:30:07 -0800

    build: add babel config and build script

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+"use strict"
+
+/**
+ * @param {import("@babel/core").ConfigAPI} api
+ * @return {import("@babel/core").TransformOptions}
+ */
+module.exports = (api) => {
+  api.cache.forever()
+
+  /** @type {import("@babel/preset-env").Options} */
+  const presetEnvConfig = {
+    modules: false,
+    targets: {
+      node: "node >= 6"
+    },
+  };
+
+  // Presets are applied right-to-left (reverse order)
+  /** @type {import("@babel/core").TransformOptions['presets']} */
+  const presets = [
+    ["@babel/preset-env", presetEnvConfig],
+  ];
+
+  return {
+    presets,
+  }
+}
