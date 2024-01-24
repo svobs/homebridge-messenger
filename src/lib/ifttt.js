26ebfd1992bf809ddd6d218bf1e64a3825d62c93
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-01-23 21:06:29 -0800

    refactor: use JSDoc types and style cleanup in IftttMessenger

--- src/lib/ifttt.js
+++ src/lib/ifttt.js
@@ -1,25 +1,30 @@
-"use strict";
+"use strict"
 
-const IFTTT = require('ifttt-webhooks-channel')
+const Ifttt = require("ifttt-webhooks-channel")
 
 module.exports = class IftttMessenger {
-    constructor(key, event, value1, value2, value3) { 
-        this.iftttKey = key
-        this.iftttEvent = event
-        this.value_1 = value1
-        this.value_2 = value2
-        this.value_3 = value3
-    }
+  /**
+   * @param {string | undefined} key
+   * @param {string | undefined} event
+   * @param {string | undefined} value1
+   * @param {string | undefined} value2
+   * @param {string | undefined} value3
+   */
+  constructor(key, event, value1, value2, value3) { 
+    this.iftttKey = key
+    this.iftttEvent = event
+    this.value_1 = value1
+    this.value_2 = value2
+    this.value_3 = value3
+  }
 
+  getRecipient() {
+    return `${this.iftttKey} (event : ${this.iftttEvent})`
+  }
 
-    getRecipient() {
-      return this.iftttKey + " (event : " + this.iftttEvent + ")"
-    }
-
-    
-    sendMessage() {      
-        const ifttt = new IFTTT(this.iftttKey)
-        ifttt.post(this.iftttEvent, [this.value_1, this.value_2, this.value_3])
-            .catch(error => new Error(error))
-    }
+  sendMessage() {
+    const ifttt = new Ifttt(this.iftttKey)
+    ifttt.post(this.iftttEvent, [this.value_1, this.value_2, this.value_3])
+      .catch(error => new Error(error))
+  }
 }
