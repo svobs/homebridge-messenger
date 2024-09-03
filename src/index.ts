b5f048fa4f849424648a74f8bce57aa36806e758
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2024-09-03 02:14:21 -0700

    Attempt to convert to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,182 @@
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service,
+} from 'homebridge';
+
+let hap: HAP;
+
+const PushOverMessenger = require('./lib/pushover.js');
+const EmailMessenger = require('./lib/email.js');
+const IftttMessenger = require('./lib/ifttt.js');
+const PushcutMessenger = require('./lib/pushcut.js');
+
+module.exports = (api: API) => {
+  hap = api.hap;
+  api.registerAccessory('homebridge-messenger', HomebridgeMessenger);
+};
+
+
+class HomebridgeMessenger implements AccessoryPlugin {
+  private readonly api: API;
+  private readonly log: Logging;
+  private readonly config: AccessoryConfig;
+  private readonly serviceMainSwitch: Service;
+  private readonly cacheDirectory: string;
+  private readonly storage = require('node-persist');
+
+  private isOn: CharacteristicValue = false;
+  private messages: [] = [];
+  private serviceMessagesSwitches: [] = [];
+  
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.api = api;
+    this.log = log;
+    this.config = config;
+
+    // Add main switch to Homebride
+    this.serviceMainSwitch = new hap.Service.Switch(this.config.name, '0');
+    this.log('Added Main Switch : ' + this.config.name);
+
+    // Initialize cache
+    this.cacheDirectory = api.user.persistPath();
+    this.storage.initSync({ dir:this.cacheDirectory, forgiveParseErrors: true });
+
+    // Get cache and validate if main switch is in cache
+    const cachedState = this.storage.getItemSync(this.config.name);
+    if((cachedState === undefined) || (cachedState === false)) { // If not in cache
+      this.isOn = false;
+      this.serviceMainSwitch.setCharacteristic(hap.Characteristic.On, false);
+      this.log('Main Switch status'+ ' : ' + false);
+    } else { // If in cache
+      this.isOn = true;
+      this.serviceMainSwitch.setCharacteristic(hap.Characteristic.On, true);
+      this.log('Main Switch status'+ ' : ' + true);
+    }
+
+    // Load configured messages
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messages = this.config.messages || [];
+    this.serviceMessagesSwitches = [];
+
+    // Iterate through configured messages
+    for (let x = 0; x < this.messages.length; x++) {
+
+      // Add switch for each message
+      const serviceMessageSwitch = new hap.Service.Switch(this.messages[x].name, x + 100);
+      this.log('Added ' + this.messages[x].type.toLowerCase() + ' : ' + this.messages[x].name);
+
+      // Add event handler for each message
+      serviceMessageSwitch.getCharacteristic(hap.Characteristic.On).on(CharacteristicEventTypes.SET, (value: CharacteristicValue, 
+        callback: CharacteristicSetCallback) => {
+        if (value===true) { // If message switch status is On
+          if (this.isOn) { // If main switch status if On
+            let message;
+            switch(this.messages[x].type.toLowerCase()) {
+
+            // Message type is email
+            case 'email': 
+              message = new EmailMessenger(this.config.services.email.recipient, 
+                this.config.services.email.smtpServer, 
+                this.config.services.email.smtpPort, 
+                this.config.services.email.smtpSecure, 
+                this.config.services.email.smtpUsername, 
+                this.config.services.email.smtpPassword, 
+                this.messages[x].name,
+                this.messages[x].text, 
+                this.messages[x].recipients);
+              break;
+
+              // Message type is pushover
+            case 'pushover':
+              message = new PushOverMessenger(this.config.services.pushover.user, 
+                this.config.services.pushover.token,
+                this.messages[x].name,
+                this.messages[x].text,
+                this.messages[x].priority,
+                this.messages[x].device,
+                this.messages[x].ttl,
+                this.messages[x].sound,
+                this.messages[x].url,
+                this.messages[x].urltitle);
+              break;
+
+              // Message type is ifttt
+            case 'ifttt':
+              message = new IftttMessenger(this.config.services.ifttt.key,
+                this.messages[x].event,
+                this.messages[x].value1,
+                this.messages[x].value2,
+                this.messages[x].value3);
+              break;  
+
+              // Message type is pushcut
+            case 'pushcut':
+              message = new PushcutMessenger(this.config.services.pushcut.apikey,
+                this.messages[x].notification,
+                this.messages[x].title,
+                this.messages[x].text,
+                this.messages[x].input,
+                this.messages[x].actions);
+              break;       
+                          
+              // Invalid message type
+            default:
+              throw new Error(this.messages[x].name  + ' : Invalid type value.');
+              break;
+            }
+            
+            this.log(this.messages[x].name  + ' : Message sent to ' + message.getRecipient());
+            message.sendMessage(); 
+          } else { // If main switch status if Off
+            this.log(this.messages[x].name + ' : Message not sent. Master switch is off.');
+          }
+
+          // Configure message switch to be stateless : will be turned off after 100 ms.
+          setTimeout(() => {
+            serviceMessageSwitch.setCharacteristic(hap.Characteristic.On, false);
+          }, 100, this.time);
+        }
+
+        callback(null);
+      });
+
+      // Add message switch to array. Array will be loaded in getServices()
+      this.serviceMessagesSwitches.push(serviceMessageSwitch);
+    }
+  
+  }
+
+
+  setOnCharacteristicHandler(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.isOn = value;
+    this.storage.setItemSync(this.config.name, value);
+    this.log('Main Switch status'+ ' : ' + value);
+    callback(null);
+  }
+
+
+  getServices () {
+    // Load configuration information for devices
+    const informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, require('./package.json').name)
+      .setCharacteristic(hap.Characteristic.SerialNumber, require('./package.json').name)
+      .setCharacteristic(hap.Characteristic.Model, require('./package.json').name)
+      .setCharacteristic(hap.Characteristic.FirmwareRevision, require('./package.json').version);
+
+    // Event handler for main switch
+    this.serviceMainSwitch.getCharacteristic(hap.Characteristic.On).on(CharacteristicEventTypes.SET, this.setOnCharacteristicHandler.bind(this)); 
+   
+    // Send all switches to Homebridge to be added
+    return [informationService, this.serviceMainSwitch, ...this.serviceMessagesSwitches];
+  }
+}
